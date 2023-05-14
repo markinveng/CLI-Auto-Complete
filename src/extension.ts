55354de7cb9b2a4a49d223119d8c2b2a45728fee
unknown <18ec092@ms.dendai.ac.jp>
2023-05-14 21:39:53 +0900

    Deleted Comment

--- src/extension.ts
+++ src/extension.ts
@@ -2,8 +2,6 @@ import * as vscode from 'vscode';
 export function activate(context: vscode.ExtensionContext) {
 	console.log('Congratulations, your extension "cli-auto-complete" is now active!');
 	let disposable = vscode.commands.registerCommand('cli-auto-complete.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from cli-auto-complete!');
 	});
 	context.subscriptions.push(disposable);
